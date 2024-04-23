a6822604b00fca9d011c8a6787819d546f3e21e4
Ar Rasyid Sarifullah <rasyidzkun@gmail.com>
2024-04-23 09:36:42 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Bookwise - Ebook Landing Page
+
+## Preview
+
+![preview1](./readme-images/preview-1.png)
+![preview2](./readme-images/preview2.png)
---
+++ assets/js/script.js
@@ -0,0 +1,57 @@
+const addEventOnElem = function (elem, type, callback) {
+  if (elem.length > 1) {
+    for (let i = 0; i < elem.length; i++) {
+      elem[i].addEventListener(type, callback);
+    }
+  } else {
+    elem.addEventListener(type, callback);
+  }
+};
+
+// Toggle Navbar
+const navbar = document.querySelector("[data-navbar]");
+const navbarLinks = document.querySelector("[data-nav-link]");
+const navToggler = document.querySelector("[data-nav-toggler]");
+
+const toggleNavbar = function () {
+  navbar.classList.toggle("active");
+  navToggler.classList.toggle("active");
+};
+
+addEventOnElem(navToggler, "click", toggleNavbar);
+
+const closeNavbar = function () {
+  navbar.classList.remove("active");
+  navToggler.classList.remove("active");
+};
+
+addEventOnElem(navbarLinks, "click", closeNavbar);
+
+// Header active
+const header = document.querySelector("[data-header]");
+
+const activeHeader = function () {
+  if (window.scrollY > 100) {
+    header.classList.add("active");
+  } else {
+    header.classList.remove("active");
+  }
+};
+
+addEventOnElem(window, "scroll", activeHeader);
+
+// Preview section
+const tabCard = document.querySelectorAll("[data-tab-card]");
+let lastTabCard = tabCard[0];
+
+const navigateTab = function () {
+  lastTabCard.classList.remove("active");
+  this.classList.add("active");
+  lastTabCard = this;
+};
+
+addEventOnElem(tabCard, "click", navigateTab);
+
+// Dynamic Copyright
+const date = new Date().getFullYear();
+document.querySelector(".cpy").innerText = date;
